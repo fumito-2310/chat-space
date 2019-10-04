cb08fe9b26efa2d77a9c0c80cecf7d68fe5a92c8
fumito-2310 <fumito2310_0111@yahoo.co.jp>
2019-10-04 20:52:29 +0900

    chat-space

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -22,9 +22,9 @@ $(document).on('turbolinks:load',function(){
     }
 
     function addUser(name,id) {
-       var html = `<div class='chat-group-user clearfix js-chat-member' id='${user.id}'>
-                     <input name='group[user_ids][]' type='hidden' value='${user.id}'>
-                     <p class='chat-group-user__name'>${user.name}</p>
+       var html = `<div class='chat-group-user clearfix js-chat-member' id='${id}'>
+                     <input name='group[user_ids][]' type='hidden' value='${id}' class="js-user">
+                     <p class='chat-group-user__name'>${name}</p>
                      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                    </div>`
         return html
@@ -35,11 +35,18 @@ $(document).on('turbolinks:load',function(){
     $('#user-search-field').on('keyup', function(e){
         var input = $("#user-search-field").val();
         
+        console.log(input);
+        var x = $(".js-user");
+        var arr = [];
+        x.each(function(i,ele){
+            arr.push(ele.value);
+        })
 
+        console.log(arr);
         $.ajax({
             type: 'GET',                
             url:  '/users',           
-            data: { keyword: input},    
+            data: { keyword: input, user_id: arr},    
             dataType: 'json'            
         })
 
@@ -59,20 +66,21 @@ $(document).on('turbolinks:load',function(){
         .fail(function(){
             alert('ユーザー検索に失敗しました');
         })
-
-        $(function(){
-         $(document).on("click",".user-search-add",function () {
-             console.log(4);
-          var id = $(this).data('user-id');
-          var name = $(this).data('user-name');
-          var html =addUser(id,name)
-          $(this).parent().remove();
-          $("#chat-group-users");
-          });
-        
-         $(document).on("click",".user-search-remove",function(){
-           $(this).parent().remove();
-            })
-        });
   });
+
+    
+    $(document).on("click",".user-search-add",function () {
+        var name = $(this).data('user-name');
+        var id = $(this).data('user-id');
+        var html =addUser(name,id)
+        $("#chat-group-users").append(html);
+        $(this).parent().remove();
+    });
+
+    $(document).on("click",".user-search-remove",function(){
+        console.log(1);
+        $(this).parent().remove();
+    });
+
 });
+
